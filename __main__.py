52c619654b83079992af65253f3e5cc9d5ede847
John Grinwis <john.grinwis@gmail.com>
2021-03-15 15:48:56 +0100

    cleanup some code

--- __main__.py
+++ __main__.py
@@ -16,39 +16,6 @@ TARGET = 3
 WEIGHT = 4
 TTL = 5
 
-# In Akamai you have an account with a unique name and account id.
-# Each account has one or more contracts each with an unique id.
-# A contract will have multiple Akamai Products assigned to it, like EdgeDNS, ION etc.
-# Within an account you have a main group and this group can have different subgroups.
-# By default the main group name is combination of account name with contract id.
-# Before we can start we need to have the approriate contract- and group id.
-# This information can be found in the Akamai Control center under "contracts"
-
-# for this demo we're going to use this group
-# can be found via  "akamai pm lg -f json"
-group_name = "GSS Training Internal-C-1IE2OHM"
-
-# select file from stack
-config = pulumi.Config()
-filename = config.require("filename")
-
-# let's first lookup the contract_id assigned to the API key coming from from .edgerc
-# to select correct api user use "pulumi config set akamai:dnsSection|akamai:propertySection [section]"
-# we should get same results as with "akamai pm lc --section [section]"
-# we only need id and we're only using first entry from contacts list
-# https://www.pulumi.com/docs/reference/pkg/akamai/getcontracts/
-# return value will be a Pulumi Output object so we can build depency graph
-contract_id = pulumi.Output.from_input(akamai.get_contracts()).contracts[0].contract_id
-
-# to debug, use pulumi.export, in this case we're only returning contract_id
-# pulumi.export("output", contract_id)
-
-# now lookup group_id: https://www.pulumi.com/docs/reference/pkg/akamai/getgroup/
-# using pulumi.Ouput object as we need to wait for results from that call when creating a zone
-group_id = pulumi.Output.from_input(
-    akamai.get_group(contract_id=contract_id, group_name=group_name).id
-)
-
 
 def create_zone(zone, contract_id, group_id):
     # when we have a contract_id and group_id, let's create a new EdgeDNS resource
@@ -80,13 +47,13 @@ def create_record(record, zone):
         return ()
 
     # set default ttl in case it's not configured or just return, check with customer as some records don't have a TTL
-    ttl = record[5] or 3600
+    ttl = record[TTL] or 3600
 
     # multiple targets are probably split with a ',' but need to double check this!
-    targets = record[3].split(",")
+    targets = record[TARGET].split(",")
 
     # our unique resource name
-    resource_name = "{}-{}-{}".format(record[1], record[2], record[3])
+    resource_name = "{}-{}-{}".format(record[NAME], record[TYPE], record[TARGET])
 
     # making use of the pulumi Output object zone.zone for the zone so we create a depency between the zone resource and dns record
     # we need to wait until resource has been created.
@@ -94,10 +61,10 @@ def create_record(record, zone):
         return pulumi.Output.from_input(
             akamai.DnsRecord(
                 resource_name,
-                recordtype=record[2],
+                recordtype=record[TYPE],
                 ttl=int(ttl),
                 zone=zone.zone,
-                name=record[1],
+                name=record[NAME],
                 targets=targets,
             )
         )
@@ -107,16 +74,16 @@ def create_record(record, zone):
         # 100 1 5061 sipfed.online.lync.com.
         # in the .csv it's configured like this
         # 1 443 sipdir.online.lync.com;100
-        srv_record = record[3].split()
+        srv_record = record[TARGET].split()
 
         return pulumi.Output.from_input(
             akamai.DnsRecord(
                 resource_name,
-                recordtype=record[2],
+                recordtype=record[TYPE],
                 ttl=int(ttl),
                 zone=zone.zone,
-                name=record[1],
-                priority=int(record[4]),
+                name=record[NAME],
+                priority=int(record[WEIGHT]),
                 weight=int(srv_record[0]),
                 port=int(srv_record[1]),
                 targets=srv_record[2].split(),
@@ -127,21 +94,52 @@ def create_record(record, zone):
         return pulumi.Output.from_input(
             akamai.DnsRecord(
                 resource_name,
-                recordtype=record[2],
+                recordtype=record[TYPE],
                 ttl=int(ttl),
                 zone=zone.zone,
-                name=record[1],
+                name=record[NAME],
                 targets=targets,
-                priority=int(record[4]),
+                priority=int(record[WEIGHT]),
             )
         )
+
+
+# In Akamai you have an account with a unique name and account id.
+# Each account has one or more contracts each with an unique id.
+# A contract will have multiple Akamai Products assigned to it, like EdgeDNS, ION etc.
+# Within an account you have a main group and this group can have different subgroups.
+# By default the main group name is combination of account name with contract id.
+# Before we can start we need to have the approriate contract- and group id.
+# This information can be found in the Akamai Control center under "contracts"
+
+# select file with zones from stack config
+# "pulumi config set filename zones.csv"
+config = pulumi.Config()
+filename = config.require("filename")
+
+# can be found via  "akamai pm lg -f json" but set if via our config set.
+group_name = config.require("group_name")
+
+# let's first lookup the contract_id assigned to the API key coming from from .edgerc
+# to select correct api user use "pulumi config set akamai:dnsSection|akamai:propertySection [section]"
+# we should get same results as with "akamai pm lc --section [section]"
+# we only need id and we're only using first entry from contacts list
+# https://www.pulumi.com/docs/reference/pkg/akamai/getcontracts/
+# return value will be a Pulumi Output object so we can build depency graph
+contract_id = pulumi.Output.from_input(akamai.get_contracts()).contracts[0].contract_id
+
+# now lookup group_id: https://www.pulumi.com/docs/reference/pkg/akamai/getgroup/
+# using pulumi.Ouput object as we need to wait for results from that call when creating a zone
+group_id = pulumi.Output.from_input(
+    akamai.get_group(contract_id=contract_id, group_name=group_name).id
+)
 
 
 zones = []
 with open(filename, newline="") as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=";")
     for row in csv_reader:
-        # only create a zone if not already confgured
+        # only try to create a zone if not already confgured
         if row[ZONE] and row[ZONE] not in zones:
             my_zone = create_zone(row[ZONE], contract_id, group_id)
             zones.append(row[ZONE])
@@ -153,4 +151,3 @@ with open(filename, newline="") as csv_file:
             my_record = create_record(row, my_zone)
 
 # pulumi.export("zone", my_zone)
-# pulumi.export("zone", my_record)
